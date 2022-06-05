03c9fcfe629db859128dea1ee1f2e3177ae471cf
parhamsis <106367433+parhamsis@users.noreply.github.com>
2022-06-05 12:15:08 +0430

    Update 120-sound.sh

--- 120-sound.sh
+++ 120-sound.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #set -e
-###############################################################################
+##################################################################################################################
 # Author	:	Erik Dubois
 # Website	:	https://www.erikdubois.be
 # Website	:	https://www.arcolinux.info
@@ -9,72 +9,31 @@
 # Website	:	https://www.arcolinuxb.com
 # Website	:	https://www.arcolinuxiso.com
 # Website	:	https://www.arcolinuxforum.com
-###############################################################################
+##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
-###############################################################################
+##################################################################################################################
 
+#https://wiki.archlinux.org/title/PipeWire
+#starting on an ArcoLinuxL iso
+#https://wiki.archlinux.org/title/PipeWire#Bluetooth_devices
 
-###############################################################################
-#
-#   DECLARATION OF FUNCTIONS
-#
-###############################################################################
+sudo pacman -S --noconfirm --needed pipewire
+sudo pacman -S --noconfirm --needed pipewire-media-session
+sudo pacman -S --noconfirm --needed pipewire-alsa
+sudo pacman -S --noconfirm --needed pipewire-jack
+sudo pacman -S --noconfirm --needed pipewire-zeroconf
 
+sudo pacman -R --noconfirm pulseaudio-equalizer-ladspa
+sudo pacman -R --noconfirm pulseaudio-alsa
+sudo pacman -R --noconfirm gnome-bluetooth blueberry
+sudo pacman -R --noconfirm pulseaudio-bluetooth
+sudo pacman -R --noconfirm pulseaudio
 
-func_install() {
-	if pacman -Qi $1 &> /dev/null; then
-		tput setaf 2
-  		echo "###############################################################################"
-  		echo "################## The package "$1" is already installed"
-      	echo "###############################################################################"
-      	echo
-		tput sgr0
-	else
-    	tput setaf 3
-    	echo "###############################################################################"
-    	echo "##################  Installing package "  $1
-    	echo "###############################################################################"
-    	echo
-    	tput sgr0
-    	sudo pacman -S --noconfirm --needed $1 
-    fi
-}
+sudo pacman -S --noconfirm --needed pipewire-pulse
+sudo pacman -S --noconfirm --needed blueberry
+sudo pacman -S --noconfirm --needed pavucontrol
+sudo systemctl enable bluetooth.service
 
-###############################################################################
-echo "Installation of sound software"
-###############################################################################
-
-list=(
-pulseaudio
-pulseaudio-alsa
-pavucontrol
-alsa-firmware
-alsa-lib
-alsa-plugins
-alsa-utils
-gstreamer
-gst-plugins-good
-gst-plugins-bad
-gst-plugins-base
-gst-plugins-ugly
-playerctl
-volumeicon
-)
-
-count=0
-
-for name in "${list[@]}" ; do
-	count=$[count+1]
-	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
-	func_install $name
-done
-
-###############################################################################
-
-tput setaf 11;
-echo "################################################################"
-echo "Software has been installed"
-echo "################################################################"
-echo;tput sgr0
+echo "Reboot now"
